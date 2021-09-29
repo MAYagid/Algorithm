87805c6789dfb5a7a1f7d7128b6b8502e13a1923
MAYagid <MAYagid@gmail.com>
2021-09-29 21:34:06 +0300

    Task3 Done

---
+++ Lesson1/Task3/Class1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task3
+{
+
+    class Class1
+    {
+        public static long Fib_Rec(int n)
+        {
+            if (n == 0) return 0;
+            else if (n == 1) return 1;
+            else return Fib_Rec(n - 1) + Fib_Rec(n - 2);
+        }
+
+        public static long Fib(int n)
+        {
+            long a1 = 0;
+            long a2 = 1;
+            for (int i = 0; i < n; i++)
+            {
+                long t = a1;
+                a1 = a1 + a2;
+                a2 = t;
+            }
+            return a1;
+        }
+    }
+}
---
+++ Lesson1/Task3/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Task3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Class1.Fib_Rec(18));
+            Console.WriteLine(Class1.Fib(18));
+        }
+    }
+}

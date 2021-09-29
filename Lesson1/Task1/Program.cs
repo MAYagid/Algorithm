1f14a99ba0c2993ed9021dac0687717062ab2fbf
MAYagid <MAYagid@gmail.com>
2021-09-29 21:01:36 +0300

    Добавьте файлы проекта.

---
+++ Lesson1/Task1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+            //Данный алгоритм, написанный по блок-схеме, неверно обрабатывает число 1
+            //С математической точки зрения единица не является простым числом.
+            //В комментариях минимальные изменения, которые можно внести, чтобы это пофиксить
+        {
+            int number = Convert.ToInt32(Console.ReadLine());
+            int d = 0;
+            int i = 2; //i = 1;
+            while (i < number) 
+            {
+                if (number % i == 0)
+                {
+                    d++;
+                }
+                i++;
+            }
+            if (d == 0) Console.WriteLine("Простое"); //if (d == 1)
+            else Console.WriteLine("Не простое"); 
+
+        }
+    }
+}

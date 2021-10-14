8c314580856585a94dac414b8e5dc150a6e49b72
MAYagid <MAYagid@gmail.com>
2021-10-14 17:50:53 +0300

    not (Task Done)

---
+++ Lesson4/Task1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Будет выполнено после 17.10");
+        }
+    }
+}

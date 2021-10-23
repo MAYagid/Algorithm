69cf0272a8bdb465d0d51d0b009cd97c4a94b434
MAYagid <MAYagid@gmail.com>
2021-10-24 00:30:53 +0300

    HomeWork6

---
+++ Lesson6/Task1/Graphs.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1
+{
+    public class Node //Вершина
+    {
+        public int Value { get; set; }
+        public List<Edge> Edges { get; set; } //исходящие связи
+
+
+    }
+
+    public class Edge //Ребро
+    {
+        public int Weight { get; set; } //вес связи
+        public Node Node { get; set; } // узел, на который связь ссылается
+    }
+
+    
+
+
+}
---
+++ Lesson6/Task1/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Xml.Linq;
+using static System.Console;
+
+namespace Task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const int n = 6;
+            int[,] g = new int[n, n]
+               {
+               { 0,1,3,0,0,0 }, //0
+               { 1,0,0,4,0,0 }, //1
+               { 3,0,0,2,0,5 }, //2
+               { 0,4,2,0,7,3 }, //3
+               { 0,0,0,7,0,9 }, //4
+               { 0,0,5,3,9,0 }, //5
+                           };
+
+            var colorV = new int[n];
+            Queue<int> queue = new Queue<int>();
+
+            int startV = 0;
+            queue.Enqueue(startV);
+            colorV[startV] = 1;
+            while (queue.Count != 0)
+            {
+                var v = queue.Dequeue();
+                Console.WriteLine($"вершина {v + 1}");
+
+                for (int i = 0; i < n; i++)
+                {
+                    if (g[v, i] != 0 && colorV[i] == 0 && i != v)
+                    {
+                        queue.Enqueue(i);
+                        colorV[i] = 1;
+                    }
+                }
+            }
+        }
+    }
+}

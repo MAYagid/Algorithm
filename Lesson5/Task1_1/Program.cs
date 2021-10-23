05cec79b0c1c14f4b7fbb40ea88efb290d3b2514
MAYagid <MAYagid@gmail.com>
2021-10-23 23:06:00 +0300

    HomeWork5 Done

---
+++ Lesson5/Task1_1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Task1_1
+{
+    class Program
+    {
+        static Node Test_tree()
+        {
+            Node root = new Node(10,
+                new Node(100,
+                    new Node(4, new Node(45), null),
+                    new Node(50)),
+                new Node(1,
+                    new Node(0),
+                    new Node(19))
+                );
+            return root;
+        }
+        static int Main(string[] args)
+        {
+            var tree = Test_tree();
+            Console.WriteLine("BFS:");
+            Node.bfs(tree);
+            Console.WriteLine("DFS:");
+            Node.dfs(tree);
+            return 0;
+        }
+        
+       
+    }
+}
---
+++ Lesson5/Task1_1/Trees.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1_1
+{
+   public class Node
+    {
+        public Node left;
+        public Node right;
+        public int Value;
+        public Node(int value, Node left, Node right)
+        {
+            this.Value = value;
+            this.left = left;
+            this.right = right;
+        }
+        public Node(int value)
+        {
+            this.Value = value;
+            this.left = null;
+            this.right = null;
+        }
+
+        public static void bfs(Node node)
+        {
+            Queue<Node> q = new Queue<Node>();
+            q.Enqueue(node);
+            while (q.Count > 0)
+            {
+                node = q.Dequeue();
+                Console.WriteLine(node.Value);
+
+                if (node.left != null)
+                    q.Enqueue(node.left);
+                if (node.right != null)
+                    q.Enqueue(node.right);
+            }
+        }
+
+        public static void dfs(Node node)
+        {
+            if (node == null) return;
+            Console.WriteLine(node.Value);
+            dfs(node.left);
+            dfs(node.right);
+        }
+
+
+    }
+
+    
+
+    
+
+
+}

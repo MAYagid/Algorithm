7bd029dd13dcafcabaeb7881b8dd760916826817
MAYagid <MAYagid@gmail.com>
2021-10-24 22:13:52 +0300

    HomeWork8 Done

---
+++ Lesson8/Task1/Class1.cs
@@ -0,0 +1,55 @@
+﻿// C# program to sort an array
+// using bucket sort
+using System;
+using System.Collections;
+using System.Collections.Generic;
+class BucketSort
+{
+    public static List<int> Sort(int[] x)
+    {
+        List<int> Array = new List<int>();
+
+        int numOfBuckets = 10;
+        List<int>[] buckets = new List<int>[numOfBuckets];
+        for (int i = 0; i < numOfBuckets; i++)
+        {
+            buckets[i] = new List<int>();
+        }
+        for (int i = 0; i < x.Length; i++)
+        {
+            int bucket = (x[i] / numOfBuckets);
+            buckets[bucket].Add(x[i]);
+        }
+
+        for (int i = 0; i < numOfBuckets; i++)
+        {
+            List<int> temp = MinSort(buckets[i]);
+            Array.AddRange(temp);
+        }
+        return Array;
+    }
+
+
+    public static List<int> MinSort(List<int> arr)
+    {
+        for (int i = 0; i < arr.Count; i++)
+        {
+            int Min = arr[i];
+            int ind = i;
+            for (int j = i; j < arr.Count; j++)
+            {
+                if (arr[j] < Min)
+                {
+                    Min = arr[j];
+                    ind = j;
+                }
+            }
+            int temp = arr[ind];
+            arr[ind] = arr[i];
+            arr[i] = temp;
+            
+        }
+
+        return arr;
+    }
+}
---
+++ Lesson8/Task1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = new int[] { 43, 17, 87, 92, 31, 6, 96, 13, 66, 62, 4 };
+
+            Console.WriteLine("Bucket Sort");        
+            List<int> sorted = BucketSort.Sort(array);
+            for (int i = 0; i < sorted.Count; i++) Console.Write($"{sorted[i]} ");
+            Console.ReadLine();
+        }
+    }
+}
